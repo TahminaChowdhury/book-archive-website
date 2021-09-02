a5e88eb2e51135d0a8676d300c7e8aaa9b01065d
TahminaChowdhury <tahmina10st@gmail.com>
2021-09-02 16:43:04 +0600

    commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const errorDiv = document.getElementById('error-div');
 const bookNumbers =document.getElementById('book-numbers');
+const spinner =document.getElementById('spinner').style.display ='none';
 
 // set event listener
 const searchResult = () => {
+    document.getElementById('spinner').style.display ='block';
     const searchField = document.getElementById('search-field');
     const searchText =searchField.value;
 
@@ -29,7 +31,7 @@ const searchResult = () => {
 
 const displaySearchResult = booksdata => {
     // console.log(booksdata)
-    
+    document.getElementById('spinner').style.display ='none';
     // display result
     const displayResult = document.getElementById('display-results');
     displayResult.innerHTML ='';
@@ -53,9 +55,9 @@ const displaySearchResult = booksdata => {
             //  img url
              const imgUrl ="https://covers.openlibrary.org/b/id/" +book.cover_i;
              div.innerHTML =`
-             <div class="card">
-                <img src="${imgUrl}-M.jpg"imclass="card-img-top" alt="...">
-                    <div class="card-body">
+             <div class="card h-100">
+                <img class="h-100" src="${imgUrl}-M.jpg"imclass="card-img-top" alt="...">
+                    <div class="card-body p-3">
                     <h5 class="card-title">${book.title}</h5>
                     <h5 class="card-title">Author-Name: ${book.author_name[0] ? book.author_name[0]: 'N/A' }</h5>
                     <h5 class="card-title">First-Publish-year: ${book.first_publish_year}</h5>
