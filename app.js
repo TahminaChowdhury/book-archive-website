7a6d7e9275eaf01f1c6d02010835dbf731cde9e4
TahminaChowdhury <tahmina10st@gmail.com>
2021-09-02 19:26:24 +0600

    commit

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ const displaySearchResult = booksdata => {
 
             // total books number
 
-                const bookNumbers =document.getElementById('book-numbers').innerText =`Books found ${books.length}`;
+                const bookNumbers =document.getElementById('book-numbers').innerText =`Total books found ${books.length}`;
             // clean books found
                 bookNumbers.innerHTML ='';
 
@@ -67,10 +67,10 @@ const displaySearchResult = booksdata => {
              const imgUrl ="https://covers.openlibrary.org/b/id/" +book.cover_i;
                 div.innerHTML =`
                 <div class="card h-100">
-                     <img class="h-100" src="${imgUrl}-M.jpg"imclass="card-img-top" alt="...">
+                     <img class="h-100" src="${imgUrl + '-M.jpg'}"imclass="card-img-top" alt="...">
                     <div class="card-body p-3">
                         <h5 class="card-title">${book.title}</h5>
-                        <h5 class="card-title">Author-Name: ${book ?.author_name?.[0]}</h5>
+                        <h5 class="card-title">Author-Name: ${book?.author_name?.[0]}</h5>
                         <h5 class="card-title">First-Publish-year: ${book.first_publish_year}</h5>
                     </div>
                </div>
